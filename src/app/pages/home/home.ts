e1483c26050bc4ae61acbe41586292e13357d4d0
protonProjects <pablowib@proton.me>
2026-04-19 21:39:56 -0500

    Feat provideZonelessChangeDetection (providers)

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DataService } from '../../services/data'; 
+import { DataService } from '../../services/data';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +10,10 @@ import { DataService } from '../../services/data';
   styleUrl: './home.css'
 })
 export class HomeComponent implements OnInit {
-  clima: any;
-  errorClima: string = '';
-  precioCafe: string = 'Cargando...';
-  errorPrecio: string = '';
+  clima = signal<any>(null);
+  errorClima = signal<string>('');
+  precioCafe = signal<string>('Cargando...');
+  errorPrecio = signal<string>('');
 
   constructor(private dataService: DataService) {}
 
@@ -22,25 +22,25 @@ export class HomeComponent implements OnInit {
       next: (res) => {
         const pronostico = res.list[0];
         const probabilidadLluvia = Math.round(pronostico.pop * 100);
-        this.clima = {
+        this.clima.set({
           nombre: res.city.name,
           probabilidad: probabilidadLluvia,
           descripcion: pronostico.weather[0].description,
           mensaje: probabilidadLluvia > 50 ? '¡Pilas! Va a llover pronto.' : 'Cielo despejado por ahora.'
-        };
+        });
       },
       error: (e) => {
         console.error('Error clima:', e);
-        this.errorClima = 'No se pudo cargar el clima. Revise su conexión.';
+        this.errorClima.set('No se pudo cargar el clima. Revise su conexión.');
       }
     });
 
     this.dataService.getPrecioCafeEnCOP(2.40).subscribe({
-      next: (res) => this.precioCafe = res,
+      next: (res) => this.precioCafe.set(res),
       error: (e) => {
         console.error('Error precio:', e);
-        this.errorPrecio = 'No se pudo cargar el precio.';
-        this.precioCafe = 'Error';
+        this.precioCafe.set('Error');
+        this.errorPrecio.set('No se pudo cargar el precio.');
       }
     });
   }
